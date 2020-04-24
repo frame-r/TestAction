009fd0c221ead63ae0064605a0deeb0cc6613a34
github-action-benchmark <github@users.noreply.github.com>
2020-04-24 15:54:27 +0000

    add C++ Benchmark (googlecpp) benchmark result for f1901c2aa1218887cf582bcb7d3a2d8a2380a156

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1587743667693,
+  "repoUrl": "https://github.com/frame-r/TestAction",
+  "entries": {
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "aaa@bbb.com",
+            "name": "k-payl",
+            "username": "xkarlois"
+          },
+          "committer": {
+            "email": "aaa@bbb.com",
+            "name": "k-payl",
+            "username": "xkarlois"
+          },
+          "distinct": true,
+          "id": "f1901c2aa1218887cf582bcb7d3a2d8a2380a156",
+          "message": "hpp->cpp in makefile",
+          "timestamp": "2020-04-24T22:50:34+07:00",
+          "tree_id": "cb452b5e54783bd5375d8583e4bafc99749b6edd",
+          "url": "https://github.com/frame-r/TestAction/commit/f1901c2aa1218887cf582bcb7d3a2d8a2380a156"
+        },
+        "date": 1587743667252,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "mat4_mult",
+            "value": 8.018564222348642,
+            "unit": "ns/iter",
+            "extra": "iterations: 86992871\ncpu: 8.015235305890757 ns\nthreads: 1"
+          },
+          {
+            "name": "mat4_inv",
+            "value": 51.401648399999544,
+            "unit": "ns/iter",
+            "extra": "iterations: 10000000\ncpu: 51.39095219999998 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
