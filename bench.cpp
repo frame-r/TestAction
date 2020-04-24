50adf56ef1033d19305102483820eedaa56aaca9
k-payl <aaa@bbb.com>
2020-04-24 23:44:43 +0700

    code fix

--- bench.cpp
+++ bench.cpp
@@ -3,28 +3,22 @@
 
 using namespace math;
 
-float _m[16] = {1,2,3,4,5,6,7,9,9,10,11,12,13,14,15,16};
-const auto iters = 2'000'000;
+float _m[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+constexpr auto iters = 2'000'000;
 
 static void mat4_mult(benchmark::State &state) {
-  for (auto _ : state) {
-    // Suppress optimization otherwise this line is removed by DCE
-    
-	for (auto i = 0; i < iters; ++i)
-	{
+  for (auto _ : state) {    
+	for (auto i = 0; i < iters; ++i) {
 		mat4 a(_m);
-		mat4 b(_m);	
-		
+		mat4 b(_m);		
 		benchmark::DoNotOptimize(a * b);
 	}
   }
 }
 
 static void mat4_inv(benchmark::State &state) {
-  for (auto _ : state) {
-	
-	for (auto i = 0; i < iters; ++i)
-	{
+  for (auto _ : state) {	
+	for (auto i = 0; i < iters; ++i) {
 		mat4 a(_m);
 		benchmark::DoNotOptimize(a.Inverse());
 	}
