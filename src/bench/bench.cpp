141d8dd2bbf8978767d7c5ba152efbea25d20419
k-payl <aaa@bbb.com>
2020-04-25 18:13:44 +0700

    manual

--- src/bench/bench.cpp
+++ src/bench/bench.cpp
@@ -7,12 +7,26 @@ float _m[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 constexpr auto iters = 2'000'000;
 
 static void mat4_mult(benchmark::State &state) {
-  for (auto _ : state) {    
+  int microseconds = state.range(0);
+  std::chrono::duration<double, std::micro> sleep_duration { static_cast<double>(microseconds) };
+  
+  for (auto _ : state) {
+	  
+    auto start = std::chrono::high_resolution_clock::now();
+	  
 	for (auto i = 0; i < iters; ++i) {
 		mat4 a(_m);
 		mat4 b(_m);		
 		benchmark::DoNotOptimize(a * b);
 	}
+	
+	auto end = std::chrono::high_resolution_clock::now();
+	
+	auto elapsed_seconds =
+      std::chrono::duration_cast<std::chrono::duration<double>>(
+        end - start);
+		
+	state.SetIterationTime(elapsed_seconds.count());
   }
 }
 
@@ -26,7 +40,7 @@ static void mat4_inv(benchmark::State &state) {
 }
 
 // Register the function as a benchmark
-BENCHMARK(mat4_mult);
+BENCHMARK(mat4_mult)->Range(1, 1<<17)->UseManualTime();
 BENCHMARK(mat4_inv);
 
 // Run the benchmark
